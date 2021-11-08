9528a913c9e23ff5c08f1403589676c0dd71e820
Philip <philipmjobe@gmail.com>
2021-11-08 13:54:13 -0500

    added math.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var moment = require("moment");
+var math = require("mathjs");
 
 console.log("Hello");
 
 var dates = moment().format("MMMM Do YYYY, h:mm:ss a");
 console.log(dates);
+
+console.log(math.log(10000, 10));
